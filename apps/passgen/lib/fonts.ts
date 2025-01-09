4bc3d89f226fc42b915b0e1ceb5060cd0fee8af2
Kian Bazza <kianbazza@Kians-MacBook-Pro.local>
2025-01-08 20:48:35 -0500

    Initial passgen site

---
+++ apps/passgen/lib/fonts.ts
@@ -0,0 +1,7 @@
+import localFont from 'next/font/local'
+
+export const berkeleyMono = localFont({
+  src: './assets/fonts/TX-02-Variable.woff2',
+  display: 'block',
+  variable: '--font-berkeley-mono',
+})
